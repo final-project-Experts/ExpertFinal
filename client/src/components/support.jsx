1f5f4a33c7575e74657031f48db684c88c4179af
Henihajer <henihajer@yahoo.fr>
2023-08-28 20:25:53 +0100

    first

---
+++ client/src/components/support.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const support=()=>(
+    <div id="support" className="carre-contenu">
+      <p>
+      Le laboratoire D'ANALYSE, laboratoire de biologie médicale du LAC MEDICAL CENTRE vous accueille dans un centre médical spécialisé sis aux berges du Lac 2 (à coté de la clinique internationale Hannibal) et comprenant la plupart des spécialités médicales, un centre de radiologie et une officine.
+      Le biologiste responsable du laboratoire et son équipe souhaitent vous accueillir le mieux possible en vous vous garantissant une qualité de service optimale.
+    
+    Le plateau technique du laboratoire assure une activité de biologie polyvalente comprenant les analyses de biochimie, hématologie, hémostase, immuno-hématologie, sérologie bactérienne et virale, microbiologie bactérienne et parasitologie. 
+    le laboratoire est ainsi spécialisé dans la biologie moléculaire et est équipé également par une paillasse de biologie moléculaire pour l'amplification du génome du VHB, VHC, HIV, Chlamydia trachomatis et Mycobacterium tuberculosis.
+    L’activité de biochimie est assurée par la chimie de Roche Diagnostics. L’activité d’hormonologie et de sérologie est assuré également par des automates de Roche diagnostics et de Biomérieux. Tous nos automates sont reliés en réseau avec le logiciel du laboratoire.
+      </p>
+      <h2>Services</h2>
+      <ul>
+          <li>Analyse au labo</li>
+          <li>Prélévement à domicile</li>
+          <li>Résultats accessible en ligne</li>
+      </ul>
+    </div>
+    );
+
+export default support;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,27 +1,74 @@
-import React, { useEffect, useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import $ from 'jquery'
 import List from './components/List.jsx'
+import acceuil from './components/acceuil.jsx'
+import entreprise from './components/entreprise.jsx'
+import support from './components/support.jsx'
+import candidat from './components/candidat.jsx'
+import offreEmploi from './components/offreEmploi.jsx'
 
 const App = () => {
   const [items, setItems] = useState([])
+  const [name, setName] = useState("")
+  const [client, setclient] = useState("")
+  const [resultat, setresultat] = useState(0)
+  const [quantity, setquantity] = useState(0)
+  const [image, setimage] = useState("")
+  const [reload, setreload] = useState(false)
   useEffect(() => {
-    $.ajax({
-      url: '/api/items',
-      success: (data) => {
-        console.log(data)
-        setItems(data)
-      },
-      error: (err) => {
-        console.log('err', err)
-      },
-    })
-  }, [])
+    //get all data 
+    axios.get("/api/items/")
+      .then((res) => {
+        console.log("response.data ", res.data)
+        setItems(res.data)
+      })
+      .catch((err) => console.error(err))
+  }, [reload])
+  const addoffre = () => {
+    axios.post("/api/items/", { name,client,resultat,image})
+      .then((res) => setreload(!reload))
+      .catch((err) => console.error(err))
+  }
+  function removeoffre(id) {
+    // Send a DELETE request to the API to remove the item with the given ID
+    axios.delete(`/api/items/${id}`)
+      .then((res) => {
+        // Remove the item from the items array
+        setItems(items.filter(item => item.idanalyses !== id));
+      })
+      .catch((err) => console.error(err));
+  }
 
   return (
     <div>
-      <h1>Item List</h1>
-      <List items={items} />
+      <div >
+      <img src="./entete.jpg"  class="full-width-image"></img>
+      </div>
+      <div>
+      <Router>
+        <nav className="navbar">
+          <Link to="/acceuil">Acceuil</Link>
+          <Link to="/candidat">Candidat</Link>
+          <Link to="/entreprise">Entreprise</Link>
+          <Link to="/offreEmploi">Offre d'emploi</Link>
+          <Link to="/support">Support</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/acceuil" element={<acceuil />} />
+          <Route path="/candidat" element={<candidat />} />
+          <Route path="/entreprise" element={<entreprise />} />
+          <Route path="/offreEmploi" element={<entreprise />} />
+          <Route path="/support" element={<support />} />
+        </Routes>
+      </Router>
+      </div>
+      <div >
+      <img src="./OIP.jpg"  class="full-width-image1"></img>
+      </div>
     </div>
   )
 }
